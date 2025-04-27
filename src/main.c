42ec95513f97154433861d93ccf80a395fbd2eba
whoopalla <whoopalla@gmail.com>
2025-04-27 19:36:25 +0300

    Trying to add linux to workflows

--- src/main.c
+++ src/main.c
@@ -16,7 +16,6 @@
 // TODO: Maybe implement kick rotations (Check if rotation is possible if you
 // move the piece away from the wall)
 
-// Testing ci
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
 #define BOARD_HEIGHT_EXTRA 2
